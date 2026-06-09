a4d2391f657618ca89e781d5976d8b083dc31081
vxntvs <pavelakhmetov147@gmail.com>
2026-06-10 01:06:25 +0300

    fix: добавлена реализация библиотеки для генерации артефакта

---
+++ include/print.hpp
@@ -0,0 +1,4 @@
+#ifndef PRINT_HPP
+#define PRINT_HPP
+void print();
+#endif
---
+++ sources/print.cpp
@@ -0,0 +1,5 @@
+#include "print.hpp"
+#include <iostream>
+void print() {
+    std::cout << "Hello from lab09 artifact!" << std::endl;
+}
